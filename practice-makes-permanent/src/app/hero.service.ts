5661e913d503c95487f4a7a05052185ab4765f49
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-05 17:39:17 -0600

    added code to heroService, and component

--- practice-makes-permanent/src/app/hero.service.ts
+++ practice-makes-permanent/src/app/hero.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HEROES } from "./mock-heroes";
+import { Hero } from './hero';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
+
 export class HeroService {
 
+  getHeroes(): Hero[] {
+    return HEROES
+  }
   constructor() { }
 }
