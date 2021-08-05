69e775ac89adb07de07392a069e9dc26e67bb3d4
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-05 12:58:48 -0600

    removed hero component

--- practice-makes-permanent/src/app/app.module.ts
+++ practice-makes-permanent/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
