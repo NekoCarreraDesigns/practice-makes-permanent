12234d2824d2dc78a8fc40cf696a17f90db90ccf
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-05 15:11:21 -0600

    created list for heroes, and added style to app

---
+++ practice-makes-permanent/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero [] = [
+    { id: 11, name: "Mr. Roboto"},
+    { id: 12, name: "Jimmy Jack-Off"},
+    { id: 13, name: "Bobby Boucher"},
+    { id: 14, name: "Billy Badass"},
+    { id: 15, name: "Cactus Jack"},
+    { id: 16, name: "Ultimate Warrior"},
+    { id: 17, name: "Macho Man Randy Savage"},
+    { id: 18, name: "Ric Flair"},
+    { id: 19, name: "Tina Belcher"},
+    { id: 20, name: "Bret + the Hitman + Hart"}
+];
