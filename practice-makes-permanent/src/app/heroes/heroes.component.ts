34ae25b9b36a0f9fa7c1f1dbcb0bd0b1a2ebaf51
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-05 20:50:16 -0600

    created dashboard, and routes

--- practice-makes-permanent/src/app/heroes/heroes.component.ts
+++ practice-makes-permanent/src/app/heroes/heroes.component.ts
@@ -12,13 +12,7 @@ export class HeroesComponent implements OnInit {
    
   heroes: Hero [] = [];
 
-  selectedHero?: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-  getHeroes(): void {
+   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
