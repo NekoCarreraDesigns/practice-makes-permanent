7174b30da3150da5591af205a88c305cc5ef854d
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-05 21:34:57 -0600

    added styles, and finalized the routes

--- practice-makes-permanent/src/app/hero-detail/hero-detail.component.ts
+++ practice-makes-permanent/src/app/hero-detail/hero-detail.component.ts
@@ -24,6 +24,12 @@ export class HeroDetailComponent implements OnInit {
   }
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHero(id).subscribe(hero => this.hero = hero)
+    this.heroService.getHero(id)
+    .subscribe(hero => this.hero = hero)
   }
+
+  goBack(): void {
+    this.location.back();
+  }
+
 }
