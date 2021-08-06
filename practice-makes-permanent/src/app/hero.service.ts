051cdac98f7b8e010085a089cbcb67d7bd278bc8
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-05 22:52:57 -0600

    started handling api, and catches for error

--- practice-makes-permanent/src/app/hero.service.ts
+++ practice-makes-permanent/src/app/hero.service.ts
@@ -3,6 +3,8 @@ import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HEROES } from "./mock-heroes";
 import { Hero } from './hero';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +15,44 @@ import { Hero } from './hero';
 export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes')
-    return heroes;
+     return this.http.get<Hero[]>(this.heroesUrl)
+     .pipe(
+       tap(_ => this.log('fetched heroes')),
+       catchError(this.handleError<Hero[]>('getHeroes', []))
+     )
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
   }
 
-  constructor(private messageService: MessageService) { }
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl,hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    )
+  }
+  httpOptions = {
+    headers: new HttpHeaders({'Content-type': 'application/json'})
+  }
+  constructor( 
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
+    private log(message: string) {
+      this.messageService.add(`HeroService: ${message}`)
+    }
+    private heroesUrl = 'api/heroes';
+    private handleError<T>(operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+        this.log(`${operation} failed: ${error.message}`);
+        return of(result as T);
+      }
+    }
+    
+    
 }
